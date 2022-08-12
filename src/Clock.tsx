cb8cabf021c6e4d00f80b5915f7609ccdabc3f96
LighterLukeCode <lighterluke@gmail.com>
2022-08-12 22:28:31 +0300

    finish

---
+++ src/Clock.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./clock.css";
+
+const Clock = () => {
+  const [seconds, setSeconds] = React.useState("");
+  const [minutes, setMinutes] = React.useState("");
+  const [hours, setHours] = React.useState("");
+
+  const getTime = async () => {
+    const response = await fetch("http://worldtimeapi.org/api/ip");
+    const data = await response.json();
+    const date = new Date(data.datetime);
+
+    let hh = date.getHours() * 30;
+    let mm = date.getMinutes() * 6;
+    let ss = date.getSeconds() * 6;
+
+    setHours(`rotateZ(${hh + mm / 12}deg)`);
+    setMinutes(`rotateZ(${mm}deg)`);
+    setSeconds(`rotateZ(${ss}deg)`);
+    console.log(seconds, hours, minutes);
+  };
+
+  React.useEffect(() => {
+    const time = setInterval(() => {
+      getTime();
+    }, 1000);
+    return () => clearInterval(time);
+  }, []);
+
+  return (
+    <>
+      <div className="half__circle"></div>
+      <div className="clock__circle">
+        <span className="clock__twelve"></span>
+        <span className="clock__three"></span>
+        <span className="clock__six"></span>
+        <span className="clock__nine"></span>
+
+        <div className="clock__hour" style={{ transform: hours }}></div>
+        <div className="clock__minutes" style={{ transform: minutes }}></div>
+        <div className="clock__seconds" style={{ transform: seconds }}></div>
+      </div>
+      <div className="half__circle"></div>
+    </>
+  );
+};
+
+export default Clock;
